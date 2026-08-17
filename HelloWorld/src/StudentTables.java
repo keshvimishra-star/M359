9a77b8205d01c2aee916b22414bc8303c808b720
Keshvi Mishra <keshvimishra@gmail.com>
2026-08-16 23:05:51 -0500

    students tables

--- HelloWorld/src/StudentTables.java
+++ HelloWorld/src/StudentTables.java
@@ -1,4 +1,14 @@
 public class StudentTables {
-
+    public static void main(String[] args) {
+        System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("== Student Points ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+        System.out.println("Name\tLab\tBonus\tTotal");
+        System.out.println("----\t---\t-----\t-----");
+        System.out.println("Ava\t45\t5\t50");
+        System.out.println("Liam\t38\t7\t45");
+        System.out.println("Sophia\t50\t10\t60");
+        System.out.println("Noah\t42\t8\t50");
+        System.out.println("Emma\t47\t6\t53");
+    }
 }
-
