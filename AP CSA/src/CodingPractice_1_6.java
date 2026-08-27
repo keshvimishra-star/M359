f56e31c8100a9156d834f3b8426535d4362d61e9
Keshvi Mishra <keshvimishra@gmail.com>
2026-08-26 19:22:07 -0500

    coding practice

---
+++ AP CSA/src/CodingPractice_1_6.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class CodingPractice_1_6 {
+    public static void main(String[] args ){
+
+        Scanner input = new Scanner(System.in);
+
+        //Problem 1
+        System.out.print("Enter three integers: ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+
+        double average = (double)(num1 + num2 + num3) / 3;
+
+        System.out.println("Numbers :" + num1 + ", " + num2 + ", " + num3);
+        System.out.println("Average :" + average);
+
+
+        //Problem 2
+        System.out.print("Enter a positive decimal value: ");
+        double decimal = input.nextDouble();
+
+        int rounded = (int)(decimal + 0.5);
+
+        System.out.println(decimal + " rounded to the nearest int is " + rounded);
+
+
+        //Problem 3
+        System.out.print("Enter the seconds: ");
+        int totalSeconds = input.nextInt();
+
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+
+        System.out.println(totalSeconds + " second is " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds");
+
+
+        //Problem 4
+        System.out.print("Enter the cents: ");
+        int cents = input.nextInt();
+
+        int quarters = cents / 25;
+        cents = cents % 25;
+
+        int dimes = cents / 10;
+        cents = cents % 10;
+
+        int nickels = cents / 5;
+        cents = cents % 5;
+
+        int pennies = cents;
+
+        System.out.println("The coins for " + (quarters * 25 + dimes * 10 + nickels * 5 + pennies) + " cents will be " + quarters + " quarter(s), " + dimes + " dime(s), " + nickels + " nickel(s), and " + pennies + " penny(ies),");
+
+        //Problem 5
+        System.out.print("Enter the number of miles: ");
+        double miles = input.nextDouble();
+
+        System.out.print("Enter the number of minutes: ");
+        double totalMinutes = input.nextDouble();
+
+        double pace = totalMinutes / miles;
+
+        int paceMinutes = (int) pace;
+        int paceSeconds = (int) ((pace - paceMinutes) * 60);
+
+        System.out.println("Average: " + paceMinutes + " min " + paceSeconds + " sec / mile");
+    }
+}
